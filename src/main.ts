e259b46231ce6cfaf85ed7a9061e06e31dd6bd70
A. Blakeslee Moore <andrew@abkslm.com>
2024-05-26 21:29:17 -0700

    Removed message from `main.js`, updated `"main"` in `package.json`.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import * as Icarus from "./icarus.js"
 
 const icarus: Icarus.Icarus = new Icarus.Icarus()
 
-icarus.init().then(r => {
-    r ? console.log("IRC Connected, icarus initialized") : console.log("IRC Failed to Connect, icarus initialized")
-})
+icarus.init().then()
 
 
